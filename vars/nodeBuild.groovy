d92c62416ccaa42440e854ffd164e98a2c7a350a
arvind yadav <arvind.yadav@codezilla.io>
2025-05-20 16:31:38 +0530

    Add basic Node.js build shared library 4

--- vars/nodeBuild.groovy
+++ vars/nodeBuild.groovy
@@ -1,11 +1,7 @@
-def call(String repoUrl = 'https://github.com/arvindyadav-codezilla/nodeapp.git') {
+def call() {
     node {
-        tools {
-            nodejs 'nodejs'
-        }
-
         stage('Checkout') {
-            git repoUrl
+            git 'https://github.com/arvindyadav-codezilla/nodeapp.git'
         }
 
         stage('Install Dependencies') {
@@ -14,14 +10,6 @@ def call(String repoUrl = 'https://github.com/arvindyadav-codezilla/nodeapp.git'
 
         stage('Run Tests') {
             sh 'npm test'
-        }
-
-        stage('Build App') {
-            sh 'npm run build'
-        }
-
-        stage('Archive Build') {
-            archiveArtifacts artifacts: 'dist/**', fingerprint: true
         }
     }
 }
